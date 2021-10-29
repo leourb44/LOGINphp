5be78ff1c60971a6b5d4b40964790deef2692c4b
leourb44 <82837996+leourb44@users.noreply.github.com>
2021-10-29 20:44:13 -0300

    Add files via upload

---
+++ aulaphp/post.php
@@ -0,0 +1,19 @@
+<pre>
+
+<?php
+
+// print_r($_POST);
+
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+$email = $_POST['email'];
+
+if($email=='unicesumar@gmail.com'&$senha==1234){
+    echo"LOGADO";
+}
+
+// echo "nome $nome, senha $senha, email $email";
+
+?>
+
+</pre>
